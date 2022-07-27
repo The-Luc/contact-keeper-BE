3691816713c2f796ba827240747122694119b215
The-Luc <thitheluc.la@gmail.com>
2022-07-27 15:30:36 +0700

    Setup auth context

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,23 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 
 function App() {
   return (
-    <ContactState>
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </div>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
