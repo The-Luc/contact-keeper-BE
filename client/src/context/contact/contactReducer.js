f94bc57d8ffa832587fb0983858ead562ea7ea40
The-Luc <thitheluc.la@gmail.com>
2022-07-28 14:35:03 +0700

    Delete contact

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -27,13 +27,13 @@ const contactReducer = (state, action) => {
       return {
         ...state,
         contacts: state.contacts.map(contact =>
-          contact.id === action.payload.id ? action.payload : contact
+          contact.id === action.payload._id ? action.payload : contact
         ),
       };
     case DELETE_CONTACT:
       return {
         ...state,
-        contacts: state.contacts.filter(contact => contact.id !== action.payload),
+        contacts: state.contacts.filter(contact => contact._id !== action.payload),
       };
     case CLEAR_CONTACTS:
       return {
